cf560beb827cdda91ced47e83ee3f82dc57e357d
Dru Ibarra <Druwerd@gmail.com>
2017-06-10 21:15:45 -0700

    Use sychronized live loops Adds 2 sleep thread

--- singles/destroy_it.rb
+++ singles/destroy_it.rb
@@ -18,53 +18,43 @@ destruction_time = 55
 destruction_level = 11 # goes to eleven
 
 # Cascade thread
-in_thread do
-  loop do
-    sync :cascade # blocks until cued
-    sample :ambi_drone, rate: -1, amp: 10
-    cue :destruction
-  end
+live_loop :cascade_loop do
+  sync :cascade # blocks until cued
+  sample :ambi_drone, rate: -1, amp: 2
+  cue :destruction
 end
 
 # Destruction thread
-in_thread do
-  loop do
-    sync :destruction
-    if one_in(2)
-      sample :bass_hard_c, decay: 5, amp: 5
-    else
-      sample :bass_drop_c, decay: 5, amp: 5
-    end
+live_loop :destruction_loop do
+  sync :destruction
+  if one_in(2) # returns true or false with a specified probability
+    sample :bass_hard_c, decay: 5
+  else
+    sample :bass_drop_c, decay: 5
   end
 end
 
 # Chords
-in_thread do
-  loop do
-    sync :chords
-    play chord(:D, :major)
-    play chord(:E, :minor)
-    play chord(:A, :major)
-  end
+live_loop :chords_loop do
+  sync :chords
+  play chord(:D, :major)
+  play chord(:E, :minor)
+  play chord(:A, :major)
 end
 
 # Pretty sounds
-in_thread do
-  loop do
-    sync :fx
-    sample :ambi_piano, sustain: 5, amp: 5
-    sample :ambi_dark_woosh, rate: 0.5
-    sample :guit_e_slide, rate: 0.5
-    play rrand(50, 95)
-  end
+live_loop :fx_loop do
+  sync :fx
+  sample :ambi_piano, sustain: 5
+  sample :ambi_dark_woosh, rate: 0.5
+  sample :guit_e_slide, rate: 0.5
+  play rrand(50, 95) # produces a float between the supplied min and max values
 end
 
 # Drums
-in_thread do
-  loop do
-    sync :drum
-    sample :drum_heavy_kick, rate: 0.5, amp: 5
-  end
+live_loop :drums_loop do
+  sync :drum
+  sample :drum_heavy_kick, rate: 0.5
 end
 
 # secondary thread loop; cues on every half beat
@@ -77,11 +67,17 @@ end
 
 # main thread loop; cues on every beat
 in_thread do
-  tick_set 1
   duration.times do
     cue :drum
-    cue :chords if (beat % 2) == 0
     sleep 1
+  end
+end
+
+# cues on every other beat
+in_thread do
+  (duration / 2).times do
+    cue :chords
+    sleep 2
   end
 end
 
